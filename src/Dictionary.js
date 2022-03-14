254e95038ff577a2201580b3d54130c904fa0265
Ana-LanitaChan <85143407+Ana-LanitaChan@users.noreply.github.com>
2022-03-14 13:38:15 -0600

    API call added
    API call added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for "${keyword}"`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
